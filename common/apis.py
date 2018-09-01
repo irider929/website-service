a9d1afbb20db1ea946d695b614cd8cc9caff98dc
catwening <mrwen22@outlook.com>
2018-09-01 23:21:17 +0800

    增加编辑用户信息接口

--- common/apis.py
+++ common/apis.py
@@ -32,3 +32,16 @@ class UserViewSet(viewsets.ViewSet):
         user.save()
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(User, id=pk)
+        if request.user == user:
+            # 修改自己的first_name
+            first_name = request.data.get('first_name')
+            if first_name:
+                user.first_name = first_name
+                user.save()
+            user = User.objects.get(id=pk)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
